034c170e027bb8e67ddd61d5ff66add14552cafa
Elyse Roth <elysemorganroth@gmail.com>
2021-10-28 11:03:04 -0400

    added sql query syntax how to to users repo

--- javaPrsServer/src/main/java/com/acme/prsserver/user/UserRepository.java
+++ javaPrsServer/src/main/java/com/acme/prsserver/user/UserRepository.java
@@ -8,6 +8,13 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	
 	Optional<User> findByUsernameAndPassword(String username, String password);
 	
+	/*how to do a query like above in more sql language. ?1/?2 are 1st and second params
+	 * @Query("select u from User u where u.username = ?1 and u.password = ?2)
+	 * User findByLogin(String username, String password);
+	 * 
+	 * @Query("select u from User u wehre u.lastname = ?1")
+	 * List<User> findByLastname(String lastname);
+	 */
 	
 	
 
